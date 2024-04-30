2fcefd4e35b646ce5c7acf31a97439f180f3eec7
Benedict Harder <benedict.harder@tum.de>
2024-04-30 11:24:16 +0200

    changed env variable

--- wsl_debian.sh
+++ wsl_debian.sh
@@ -10,15 +10,15 @@ apt install git texlive-full python3 python3-pip python3-venv neofetch lf curl -
 echo 'export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\h:\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;27m\]\W\[$(tput sgr0)\] \[$(tput bold)\]\\$ \[$(tput sgr0)\]"' >> $HOME/.bashrc
 
 # insert aliases here
-echo "alias ll=\"ls -la\"" >> $HOME/.bashrc
-echo "alias repos=\"cd /mnt/c/dev/repos\"" >> $HOME/.bashrc 
-echo "alias exe=\"explorer.exe\"" >> $HOME/.bashrc # shortcut for windows explorer
-echo "alias pyvenv=\"python3 -m venv\"" >> $HOME/.bashrc
+echo "alias ll=\"ls -la\"" >> /home/$USER/.bashrc
+echo "alias repos=\"cd /mnt/c/dev/repos\"" >> /home/$USER/.bashrc 
+echo "alias exe=\"explorer.exe\"" >> /home/$USER/.bashrc # shortcut for windows explorer
+echo "alias pyvenv=\"python3 -m venv\"" >> /home/$USER/.bashrc
 
 # reload the bash config
-sh $HOME/.bashrc
+sh /home/$USER/.bashrc
 
-mkdir $HOME/repos
+mkdir /home/$USER/repos
 
 # git configuration, change accordingly
 git config --global user.name "Benedict Harder"
